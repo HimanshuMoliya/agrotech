3be1f3737ad3f8c16fc112d2e61806076dbfa9ec
lyricalboii <lyricalboii001@gmail.com>
2022-08-24 21:39:59 +0530

    commit

--- index1.php
+++ index1.php
@@ -65,5 +65,11 @@ else{
     <?php
         include "assets/navbar/index.php";
     ?>
+    <script>
+        var loader = document.getElementById("preloader");
+        window.addEventListener("load",function(){
+            loader.style.display = "none";
+        })
+    </script>
 </body>
 </html>
