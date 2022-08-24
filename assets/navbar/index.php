9a10280dac6365af296aba15c7e02ba97163bca9
lyricalboii <lyricalboii001@gmail.com>
2022-08-24 21:14:34 +0530

    commit

--- assets/navbar/index.php
+++ assets/navbar/index.php
@@ -67,7 +67,7 @@ else{
     <nav class="navbar navbar-expand-lg bg-light" id="navbar">
         <div class="container-fluid">
             <div id="logo_div">
-          <a class="navbar-brand" href="#"><img src="/agrotech" id="logo"></a>
+          <a class="navbar-brand" href="#"><img src="/assets/css/img/logo.png" id="logo"></a>
         </div>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
