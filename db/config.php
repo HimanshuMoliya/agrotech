5cdb3e71846f501c45d1aa632000f93cfa51125f
lyricalboii <lyricalboii001@gmail.com>
2022-09-01 13:49:54 +0530

    commit

--- db/config.php
+++ db/config.php
@@ -6,7 +6,7 @@ session_start();
 // init configuration
 $clientID = '837007103942-jr4t61mr2h5d007p13sitc7ene3lo0ba.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUri = 'https://aggregate-agro.herokuapp.com/';
+// $redirectUri = 'https://aggregate-agro.herokuapp.com/';
 // $redirectUri = 'http://localhost/agrotech/index.php';
 
 // create Client Request to access Google API
