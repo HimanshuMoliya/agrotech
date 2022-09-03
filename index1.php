7bf8cd2f616beb6b6d7eccb64cfa9a8bea1bcd2e
roshan538 <thummarroshan07@gmail.com>
2022-09-03 13:26:48 +0530

    commit

--- index1.php
+++ index1.php
@@ -50,6 +50,7 @@ else{
     <link rel="icon" type="image/x-icon" href="assets/css/img/logo.png">
     <link rel="stylesheet" href="workernav.css">
     <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
 
     <style>
