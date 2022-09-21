924cdb963b8488e01f4b6747045102aa8d6d5498
lyricalboii <lyricalboii001@gmail.com>
2022-09-21 20:12:32 +0530

    commit

--- myprofile1.php
+++ myprofile1.php
@@ -45,6 +45,8 @@ else{
         crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="footer.css">
+        <link rel="icon" type="image/x-icon" href="assets/css/img/logo.png">
+
     <style>
         * {
             margin: 0;
